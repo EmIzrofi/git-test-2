b04d26d420433843e692afc0b9765b3f1bd6be56
Em Izrofi <emizrofi001@gmail.com>
2020-05-10 23:39:46 +0700

    Menambahkan list item pada file index.php

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
 <body>
     <h1>Hello world!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nulla sapiente perspiciatis voluptas ea impedit, nam perferendis natus laborum at dicta sit hic iste mollitia beatae! Aliquam dicta eos illo!</p>
+    <ul>
+        <li>Testimonial</li>
+    </ul>
 </body>
 
 </html>
